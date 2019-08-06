3de676e3925650b474f9ddcc13343c332e87c5df
Tyson Thomason <tthomason@mailserviceslc.com>
2019-08-06 15:42:15 -0500

    finished 5.2

---
+++ week-5/thomason-exercise-5.2/thomason-md-toolbar/src/app/app.component.ts
@@ -0,0 +1,20 @@
+/*
+=====================================
+  ; Title: app.component.ts
+  ; Author: William Thomason
+  ; Date: August 6 2019
+  ; Modified By: William Thomason
+  ; Description: app.component.ts
+======================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'thomason-md-toolbar';
+}
---
+++ week-5/thomason-exercise-5.2/thomason-md-toolbar/src/app/app.module.ts
@@ -0,0 +1,36 @@
+/*
+=====================================
+  ; Title: app.module.ts
+  ; Author: William Thomason
+  ; Date: August 6 2019
+  ; Modified By: William Thomason
+  ; Description: app.module.ts
+======================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
